25d3848842c1bee614d1749b4d31f1a8aaf5a695
kimsuji <skme1103@naver.com>
2023-10-11 21:36:31 +0900

    231011_1.6_end

--- pages/#06_NavBar.js
+++ pages/#06_NavBar.js
@@ -1,4 +1,4 @@
-// #07_Custom App
+// #06_Styles JSX : 어플리케이션에 styles를 추가하는 또 다른 방식, Next.js 고유 방식
 
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -9,6 +9,7 @@ export default function NavBar(){
         <nav>
             <Link href="/" className={router.pathname === "/" ? "active" : ""}>Home</Link>
             <Link href="/about" className={router.pathname === "/about" ? "active" : ""}>About</Link>
+            {/* styles jsx는 현재 페이지에서만 적용된다 */}
             <style jsx global>{` 
                 nav {
                     background-color: tomato;
